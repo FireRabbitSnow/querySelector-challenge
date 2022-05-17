b9affcd547aa0cfea9854ee30f1f9f67101b9e8a
Renate W <Renate.Whitaker@gmail.com>
2022-05-16 20:18:23 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("hi!");
+var mainTitle = document.querySelector("h1");
+console.log(mainTitle);
+mainTitle.innerText = "Yay, Javscript!";
+console.log(mainTitle);
+
+var body = document.querySelector("body");
+console.log(body);
+body.style.backgroundColor = "#FEF2F1";
